87c00d657d0a9d1e63e3826067bb41d5ac363721
Gaboa Ventura <gaboa@venturadental.com>
2026-06-27 12:42:59 -0600

    feat(model): add cita model with CRUD and conflict detection

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,3 +3,4 @@ export * as pacienteModel from './paciente.js';
 export * as pagoModel from './pago.js';
 export * as comprobanteModel from './comprobante.js';
 export * as tratamientoModel from './tratamiento.js';
+export * as citaModel from './cita.js';
